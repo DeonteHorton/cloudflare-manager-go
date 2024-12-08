ea7e6e8b0b5ef07285579b948770d810f4f75966
Deonte Horton <deontehorton@Mac.lan>
2024-12-08 17:34:43 -0600

    WIP, file can be uploaded to R2 storage

---
+++ go.mod
@@ -0,0 +1,34 @@
+module cloudflare-manager/v
+
+go 1.23.4
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.32.6 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.7 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.28.6 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.47 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.25 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.38.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.4.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.10.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.18.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.71.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.24.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.28.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.33.2 // indirect
+	github.com/aws/smithy-go v1.22.1 // indirect
+	github.com/cloudflare/cloudflare-go v0.111.0 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/net v0.31.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	golang.org/x/time v0.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,36 +2,75 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/cloudflare/cloudflare-go"
+	"github.com/aws/aws-sdk-go-v2/aws"
+  "github.com/aws/aws-sdk-go-v2/config"
+  "github.com/aws/aws-sdk-go-v2/credentials"
+  "github.com/aws/aws-sdk-go-v2/service/s3"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
-	// url := os.Getenv("CLOUDFLARE_URL")
-	email := os.Getenv("CLOUDFLARE_EMAIL")
+	accountid := os.Getenv("CLOUDFLARE_ACCOUNT_ID")
+	bucket := os.Getenv("CLOUDFLARE_BUCKET")
 	key := os.Getenv("CLOUDFLARE_KEY")
-	// keyid := os.Getenv("CLOUDFLARE_KEYID")
-	// secret := os.Getenv("CLOUDFLARE_SECRET")
-	//	bucket := os.Getenv("CLOUDFLARE_BUCKET")
+	secret := os.Getenv("CLOUDFLARE_SECRET")
 
-	api, err := cloudflare.New(key, email)
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+    config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(key, secret, "")),
+    config.WithRegion("auto"),
+  )
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+      o.BaseEndpoint = aws.String(fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountid))
+  })
+
+  listObjectsOutput, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+    Bucket: &bucket,
+  })
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  file, err := os.Open("hello.txt")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to open file, %v", err)
+	}
+	defer file.Close()
+
+  uploadInput := &s3.PutObjectInput{
+  	Bucket: aws.String(bucket),
+  	Key: aws.String(file.Name()),
+  	Body: file,
+  }
+
+	_, err = client.PutObject(context.TODO(), uploadInput)
+
+	if err != nil {
+		log.Fatal("failed to upload file", err)
 	}
 
-	// Most API calls require a Context
-	ctx := context.Background()
 
-	// Fetch user details on the account
-	u, err := api.UserDetails(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
+  for _, object := range listObjectsOutput.Contents {
+    obj, _ := json.MarshalIndent(object, "", "\t")
+    fmt.Println(string(obj))
+  }
 
-	// Print user details
-	fmt.Println(u)
+
+  listBucketsOutput, err := client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  for _, object := range listBucketsOutput.Buckets {
+    obj, _ := json.MarshalIndent(object, "", "\t")
+    fmt.Println(string(obj))
+  }
+
 }
